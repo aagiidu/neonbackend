62a3e1135d455cd8489322889d7876936dd1edb7
altangerel.d <altangerel.d@ddishtv.mn>
2022-12-21 17:03:25 +0800

    range problem

--- index.js
+++ index.js
@@ -24,10 +24,8 @@ app.get("/video/:type/:name/:size", function (req, res) {
   // const allowed = ['http://localhost/', 'https://neontoon.mn/', 'https://www.neontoon.mn/'];
   // if(!allowed.includes(req.headers.referer)) return res.status(403).send("Хандах эрхгүй!");
   const range = req.headers.range;
-  console.log('HEaders', req.headers)
-  if (!range) {
-    res.status(400).send("Requires Range header");
-  }
+  //console.log('HEaders', req.headers)
+  if (!range) range = 'bytes=0-'
 
   // get video stats (about 61MB)
   // Zamaa zasna!
